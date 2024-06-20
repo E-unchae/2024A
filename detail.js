708f195faac6ab65d6bdc6ac8265b548ab9a998a
E-unchae <oec0307@gmail.com>
2024-06-21 03:23:32 +0900

    Update detail.js

--- detail.js
+++ detail.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
     $('a').on("click", function(e){
         let sid = $(this).attr('menu_sid');
         if(sid){
-            location.href="/index.html";
+            location.href="/https://e-unchae.github.io/2024A/";
         }
     });
 });
